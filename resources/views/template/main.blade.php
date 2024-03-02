8aac82e89613c8d119edae5820a7af482d4bb6d9
Aditya Restu <adityarestuhudayana@gmail.com>
2024-03-02 14:15:38 +0700

    mengganti icon web & melakukan beberapa perbaikan tampilan

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>To Do List</title>
+    {{-- Icon Website --}}
+    <link href='img/laravel.png' rel='shortcut icon'>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -12,10 +15,13 @@
 
 <body>
 
+    {{-- Navbar --}}
     @include('template.navbar')
 
+    {{-- Main content --}}
     <div class="container">
 
+        {{-- Sidebar --}}
         @include('template.sidebar')
 
         @yield('container')
--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -1,22 +1,15 @@
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
     aria-labelledby="offcanvasWithBothOptionsLabel">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Menu</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
         <ul class="list-group">
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                A list item
+                <a href="/" class="text-decoration-none text-dark"><i
+                        class="bi bi-house-check-fill me-2"></i>Home</a>
                 <span class="badge text-bg-primary rounded-pill">14</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                A second list item
-                <span class="badge text-bg-primary rounded-pill">2</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                A third list item
-                <span class="badge text-bg-primary rounded-pill">1</span>
             </li>
         </ul>
     </div>
