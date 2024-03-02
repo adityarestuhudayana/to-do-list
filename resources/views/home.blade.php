820777a46a551d5a3bbd87a07afceb9a5c7035ad
Aditya Restu <adityarestuhudayana@gmail.com>
2024-03-02 14:03:19 +0700

    membuat template design & membuat tampilan awal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('template.main')
+
+@section('container')
+    <h1>Hello World!</h1>
+@endsection
---
+++ resources/views/template/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar bg-body-tertiary mb-3">
+    <div class="container">
+        <button class="btn btn-danger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions"
+            aria-controls="offcanvasWithBothOptions"><i class="bi bi-list"></i></button>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-danger" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
